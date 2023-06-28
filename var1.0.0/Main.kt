4184c391f4c5d4c0aebce894045017287f3a8f9d
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2023-06-28 21:22:53 +0900

    var1.0.0

---
+++ var1.0.0/Main.kt
@@ -0,0 +1,17 @@
+package com.github.Ringoame196
+
+import com.github.Ringoame196.Listeners.Events
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+    override fun onEnable() { // 読み込み時
+        super.onEnable()
+        val plugin = this
+        val event = Events(plugin)
+        server.pluginManager.registerEvents(event, this)
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+}
